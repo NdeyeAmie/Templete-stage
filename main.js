31cd1c5077f267a640d3f9a64915b37582b845ed
Ndeye456 <tndeyeamie456@gmail.com>
2023-11-09 17:36:42 +0000

    first commit

---
+++ main.js
@@ -0,0 +1,10 @@
+document.getElementById('voirToutesCategories').addEventListener('click', function() {
+    // Code à exécuter lorsque le bouton est cliqué
+    afficherToutesCategories();
+});
+
+function afficherToutesCategories() {
+    // Code pour afficher toutes les catégories
+    // Par exemple, afficher des éléments masqués ou charger davantage de catégories via une requête AJAX
+    console.log("Afficher toutes les catégories !");
+}
