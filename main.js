834e944c81da4cf224924d047d08a5ddbad01cf8
Ndeye456 <tndeyeamie456@gmail.com>
2023-11-10 09:30:19 +0000

    commit

--- main.js
+++ main.js
@@ -8,3 +8,20 @@ function afficherToutesCategories() {
     // Par exemple, afficher des éléments masqués ou charger davantage de catégories via une requête AJAX
     console.log("Afficher toutes les catégories !");
 }
+
+const trendingSwiper = new Swiper(".trending__swiper", {
+    loop: true,
+    slidesPerView: "auto",
+    spaceBetween: 50,
+  });
+  
+  const testimonialSwiper = new Swiper(".testimonial__swiper", {
+    loop: true,
+    spaceBetween: 30,
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+  });
+
+  
